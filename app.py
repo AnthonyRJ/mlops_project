60ad92b9454fe3ad2b889a3957b4e18258a7e2a4
Anthony REINO JOAQUIM <anthonyjoaquim40@gmail.com>
2023-02-25 01:35:17 +0100

    refactor + modify the flask app

--- app.py
+++ app.py
@@ -5,25 +5,29 @@ from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
+app.static_folder = './front/static'
+app.template_folder = './front/templates'
+
 # Load the machine learning model
-model = load_model('model.h5')
+#model = load_model('model.h5')
 
 @app.route('/', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         # get user inputs from the form
         title = request.form['title']
-        genres = request.form['genres']
+        genres = request.form['genre[]']
         description = request.form['description']
-        anime_type = request.form['anime_type']
-        producer = request.form['producer']
-        studio = request.form['studio']
+        anime_type = request.form['type[]']
+        producer = request.form['producer[]']
+        studio = request.form['studio[]']
 
         # Use the model to make a prediction
-        prediction = model.predict([[title, genres, description, anime_type, producer, studio]])
+        #prediction = model.predict([[title, genres, description, anime_type, producer, studio]])
 
         # Return the predicted rating as a JSON response
-        return jsonify({'prediction': prediction[0][0]})
+        #return jsonify({'prediction': prediction[0][0]})
+        return jsonify({'prediction': 10})
 
     # If the request method is GET, render the home template
     return render_template('home.html')
